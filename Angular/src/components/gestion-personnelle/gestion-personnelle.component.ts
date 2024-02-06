7d2c6e691f16b5a03a77551bf9b600431e77abe5
Jerry ANDRIANIAINA <99274824+JerryA-1088@users.noreply.github.com>
2024-02-06 22:10:19 +0300

    Gestion personnelle et service
    Interface manager pour gérer les personnelle et les services

---
+++ Angular/src/components/gestion-personnelle/gestion-personnelle.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-gestion-personnelle',
+  standalone: true,
+  imports: [],
+  templateUrl: './gestion-personnelle.component.html',
+  styleUrl: './gestion-personnelle.component.css'
+})
+export class GestionPersonnelleComponent {
+
+}
