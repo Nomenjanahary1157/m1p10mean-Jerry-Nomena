afa031fcb22a6724c7a66ffd832e086da7f1af5e
Nomenjanahary1157 <89528663+Nomenjanahary1157@users.noreply.github.com>
2024-02-22 12:05:49 +0300

    ajout de la barre de Recherche

--- Front-Angular/src/app/acceuil/acceuil.component.ts
+++ Front-Angular/src/app/acceuil/acceuil.component.ts
@@ -7,9 +7,95 @@ import { Router } from '@angular/router';
   styleUrls: ['./acceuil.component.css']
 })
 export class AcceuilComponent {
+  form: any = {
+    recherche: null,
+  };
+
+  showEmptyAlert: boolean = false;
+  showNotFoundAlert: boolean = false;
+  
   constructor(private router: Router) {}
 
   goToAcceuil() {
     this.router.navigate(['/acceuil']);
   }
+
+  rechercher() {
+    const { recherche } = this.form;
+    console.log(recherche);
+
+    if (!recherche) {
+      console.log("Veuillez entrer quelque chose à rechercher.");
+      this.showEmptyAlert = true;
+      this.showNotFoundAlert = false;
+      return;
+    }
+    else {
+      this.showEmptyAlert = false;
+    }
+    if (!this.rechercherManucure(recherche) && !this.rechercherPedicure(recherche) && !this.rechercherCheveux(recherche)) {
+      this.showNotFoundAlert = true;
+  } else {
+      this.showNotFoundAlert = false; 
+  }
+
+    if (this.rechercherManucure(recherche)) return;
+    if (this.rechercherPedicure(recherche)) return;
+    if (this.rechercherCheveux(recherche)) return;
+
+    console.log("Aucun service trouvé pour la recherche: " + recherche);
+  }
+
+  rechercherManucure(recherche: string): boolean {
+    const manucureServices: string[] = [
+      'La manucure classique',
+      'La manucure classique express',
+      'La manucure brésilienne',
+      'La manucure japonaise',
+      'La manucure russe'
+    ];
+
+    if (manucureServices.includes(recherche)) {
+      this.router.navigateByUrl('/dragDrop');
+      return true;
+    }
+    return false;
+  }
+
+  rechercherPedicure(recherche: string): boolean {
+    const pedicureServices: string[] = [
+      'BLANC CRAIE',
+      'RENCH PÉDICURE',
+      'ONGLES ROUGES.',
+      'VERT ACIDE',
+      'JAUNE FLUO',
+      'MAINS ET PIEDS DÉPAREILLÉS'
+    ];
+
+    if (pedicureServices.includes(recherche)) {
+      this.router.navigateByUrl('/dragDropPeducure');
+      return true;
+    }
+    return false;
+  }
+
+  rechercherCheveux(recherche: string): boolean {
+    const cheveuxServices: string[] = [
+      'Carré plongeant',
+      'Tresse Pour Cheveux',
+      'Coupe courte avec frange',
+      'Coupe courte ondulée',
+      'Coupe dégradée cheveux longs',
+      'Coupe avec Natte',
+      'Cheveux bouclés',
+      'Cheveux frisés'
+    ];
+
+    if (cheveuxServices.includes(recherche)) {
+      this.router.navigateByUrl('/dragDropCheveux');
+      return true;
+    }
+    return false;
+  }
+
 }
