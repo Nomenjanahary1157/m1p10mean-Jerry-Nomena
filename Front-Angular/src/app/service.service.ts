aaf138fd21958195a5f9ea98e7f28a7f0480bc52
Nomenjanahary1157 <89528663+Nomenjanahary1157@users.noreply.github.com>
2024-02-29 20:26:13 +0300

    service

--- Front-Angular/src/app/service.service.ts
+++ Front-Angular/src/app/service.service.ts
@@ -1,23 +1,41 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders,HttpResponse  } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { base_url} from './app.module';
-import { ActivatedRoute,Router } from '@angular/router';
-const httpOptions = {
-  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
-};
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
+import { BehaviorSubject, Observable, catchError, throwError } from 'rxjs';
+import { base_url } from './app.module';
+import { Router } from '@angular/router';
+
 @Injectable({
   providedIn: 'root'
 })
-
 export class ServiceService {
   isLoggedIn: boolean = false;
   user: any;
+  private isConnectedSubject: BehaviorSubject<{ isConnected: boolean, client: any }>;
+  public isConnected$: Observable<{ isConnected: boolean, client: any }>;
 
-  constructor(private http: HttpClient, public router:Router) { }
+  constructor(private http: HttpClient, public router: Router) {
+    this.isConnectedSubject = new BehaviorSubject<{ isConnected: boolean, client: any }>({ isConnected: false, client: null });
+    this.isConnected$ = this.isConnectedSubject.asObservable();
+  }
+
+  getClientArgent(): number {
+    return this.user?.argent || 0;
+  }
+
+  set isConnected(data: { isConnected: boolean, client: any }) {
+    this.isConnectedSubject.next(data);
+  }
+
+  get isConnected(): { isConnected: boolean, client: any } {
+    return this.isConnectedSubject.value;
+  }
 
   getRendezVous(): Observable<any[]> {
-    return this.http.get<any[]>(base_url+'/rdvs');
+    return this.http.get<any[]>(base_url + '/rdvs');
+  }
+
+  getRendezVousByClient(id: number): Observable<any[]> {
+    return this.http.get<any[]>(`${base_url}/rdvs/${id}`);
   }
 
   getClientbyID(id: number): Observable<any> {
@@ -51,37 +69,86 @@ export class ServiceService {
   getEmployer(): Observable<any> {
     return this.http.get<any>(`${base_url}/Employers`);
   }
+
   getService(): Observable<any> {
     return this.http.get<any>(`${base_url}/services`);
   }
 
-  connexion(username: string, mdp: string) :Observable<any> {
+  getLastServEmployer(): Observable<any> {
+    return this.http.get<any>(`${base_url}/servElatest`);
+  }
+
+  insertServEmployer(idServEmployer: number, idService: number, idEmployer: number): Observable<any> {
+    const body = {
+      idServEmployer: idServEmployer,
+      idService: idService,
+      idEmployer: idEmployer
+    };
+
+    return this.http.post<any>(`${base_url}/ServEmployers`, body)
+      .pipe(
+        catchError(this.handleError)
+      );
+  }
+
+  insertRDV(id: number, idClient: number, idServEmployer: number, date: Date): Observable<any> {
+    const body = {
+      id: id,
+      idClient: idClient,
+      idServEmployer: idServEmployer,
+      date: date
+    };
+
+    return this.http.post<any>(`${base_url}/rdvs`, body)
+      .pipe(
+        catchError(this.handleErro)
+      );
+  }
+
+  private handleError(error: HttpErrorResponse) {
+    if (error.error instanceof ErrorEvent) {
+    } else {
+    }
+    return throwError('');
+  };
+
+  private handleErro(error: HttpErrorResponse) {
+    if (error.error instanceof ErrorEvent) {
+    } else {
+    }
+    return throwError('');
+  };
+
+  connexion(username: string, mdp: string): Observable<any> {
     return this.http.get<any>(`${base_url}/clients/${username}/${mdp}`);
   }
 
   login(user: any) {
     this.isLoggedIn = true;
     this.user = user;
+    this.isConnected = { isConnected: true, client: user.id };
   }
 
   logout() {
     this.isLoggedIn = false;
-    this.user = null;
+    this.isConnected = { isConnected: false, client: null };
   }
 
-  inscription(id:number, nom: string, prenom: string, username: string, mdp: string): Observable<any> {
-    const clientData = {id, nom, prenom, username, mdp };
+  updateArgent(clientId: number, argent: number): Observable<any> {
+    const body = {
+      argent: argent
+    };
+  
+    return this.http.put<any>(`${base_url}/clients/${clientId}/update-argent`, body)
+      .pipe(
+        catchError(this.handleError)
+      );
+  }
+  
+  
+
+  inscription(id: number, nom: string, prenom: string, username: string, mdp: string, argent: number): Observable<any> {
+    const clientData = { id, nom, prenom, username, mdp, argent };
     return this.http.post<any>(`${base_url}/clients`, clientData);
   }
-
-  addPers(idEmployer:number, nomEmployer: string, prenomEmployer: string, salaire: number, dateEmbauche: Date): Observable<any> {
-    const employerData = {idEmployer, nomEmployer, prenomEmployer, salaire, dateEmbauche };
-    return this.http.post<any>(`${base_url}/employers`, employerData);
-  }
-
-  addService(idService:number, nomService: string, prix: number, dure: number, nomOption: Date): Observable<any> {
-    const serviceData = {idService, nomService, prix, dure, nomOption };
-    return this.http.post<any>(`${base_url}/services`, serviceData);
-  }
-
 }
